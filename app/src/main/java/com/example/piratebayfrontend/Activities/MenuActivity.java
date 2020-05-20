59e2632246f89e56a55bad4bdfa3a321ae59d189
Mariana Cardozo <mcardozocosta@hotmail.com>
2020-05-20 15:42:09 -0400

    Agregar usuario

--- app/src/main/java/com/example/piratebayfrontend/Activities/MenuActivity.java
+++ app/src/main/java/com/example/piratebayfrontend/Activities/MenuActivity.java
@@ -1,10 +1,19 @@
 package com.example.piratebayfrontend.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.piratebayfrontend.MainActivity;
+import com.example.piratebayfrontend.Model.CredentialModel;
 import com.example.piratebayfrontend.R;
+
+import java.text.Normalizer;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -12,5 +21,16 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        CardView cvUsuarios = findViewById(R.id.cvUsuarios);
+
+        cvUsuarios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MenuActivity.this, FormActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

36705e2efab2f6c7c1461095247732a1a162aa85
Mariana Cardozo <mcardozocosta@hotmail.com>
2020-06-14 21:29:47 -0400

    Mostrando a los usuarios en una tabla

--- app/src/main/java/com/example/piratebayfrontend/Activities/UsersListActivity.java
+++ app/src/main/java/com/example/piratebayfrontend/Activities/UsersListActivity.java
@@ -2,9 +2,13 @@ package com.example.piratebayfrontend.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.UserManager;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
+import com.example.piratebayfrontend.Clases.Tabla;
 import com.example.piratebayfrontend.Clases.TokensControl;
 import com.example.piratebayfrontend.Controladores.RefreshTokenController;
 import com.example.piratebayfrontend.Controladores.UserController;
@@ -18,10 +22,16 @@ import java.util.Map;
 
 public class UsersListActivity extends AppCompatActivity {
 
+    Tabla tabla;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_list);
+
+        bindUI();
+
+        tabla.agregarCabecera(R.array.cabecera_tabla);
 
         Map<String, String> tokens = TokensControl.retrieveTokens(getApplicationContext());
         UserController userController= new UserController(tokens.get("authentication"));
@@ -29,9 +39,24 @@ public class UsersListActivity extends AppCompatActivity {
             @Override
             public void onSuccess(boolean value, ArrayList<UserModel> usersList) {
                 if(value && usersList!=null){
-                    System.out.println(usersList);
+                    agregarUsuarios(usersList);
                 }
             }
         });
+    }
+
+    private void bindUI(){
+        tabla= new Tabla((TableLayout)findViewById(R.id.layoutTabla), this);
+    }
+
+    private void agregarUsuarios(ArrayList<UserModel> usersList){
+        for(int i=0; i<usersList.size(); i++){
+            ArrayList<String> users = new ArrayList<String>();
+            users.add(Integer.toString(usersList.get(i).getUserId()));
+            users.add(usersList.get(i).getUsername());
+            users.add(usersList.get(i).getEmail());
+            users.add(usersList.get(i).getPhoneNumber());
+            tabla.agregarFilaTabla(users);
+        }
     }
 }
